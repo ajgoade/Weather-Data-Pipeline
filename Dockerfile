1fb3a18e1f7654c01d276731895ebab69fe5410e
Andrew Goade <ajg135@gmail.com>
2024-03-07 14:37:29 -0600

    cleanup

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM apache/airflow:2.5.3-python3.10
-
-ENV AIRFLOW_HOME=/opt/airflow
-WORKDIR $AIRFLOW_HOME
-
-# Copy and install Python dependencies
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
--- airflow/dags/s3test.py
+++ airflow/dags/s3test.py
@@ -34,7 +34,7 @@ def s3_upload():
     for file in new_files:
         s3_hook.load_file(
             filename=file,
-            key=file.rsplit('/', 1)[-1],
+            key=file.rsplit('/', 1)[-2],
             bucket='isd-weather',
             replace=True
         )
